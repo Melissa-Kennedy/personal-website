d3a10c88be09fe75e1120c099d68299f3d29b284
Melissa <melissa.4.kennedy@gmail.com>
2021-01-02 12:40:43 -0500

    Initial set up

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div class="bg-yellow-400 text-4xl font-quicksand sm:text-sm">This element was styled with Tailwind, and the font is from Google Fonts</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
