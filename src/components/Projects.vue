9a19e37ba65961f39e2d531d3e7dd76fea5f0df2
Melissa <melissa.4.kennedy@gmail.com>
2021-01-13 11:03:58 -0500

    Create projects section

---
+++ src/components/Projects.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <br />
+    <h1>Projects</h1>
+    <h2>Pantry</h2>
+    <span>
+      <br />• Recipe app using vue.js, django rest frame work, and postgres
+    </span>
+    <h2>Namaste</h2>
+    <span>
+      <br />• Yoga sequence builder using vue.js and Django
+      <br />• Submitted to the 2020 winter Hacklympics with partner
+    </span>
+    <h2>Hacktober Fest</h2>
+    <span>
+      <br />• Contributed to 4 pull requests on Github
+    </span>
+    <h2>MelissaKennedy.ca</h2>
+    <span>
+      <br />• Personal webstie built using vue.js and Tailwind
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Projects"
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
       <About class="z-10" />
       <Education class="z-10" />
       <Experience class="z-10" />
+      <Projects class="z-10" />
     </div>
   </div>
 </template>
@@ -21,7 +22,7 @@ import Navbar from "@/components/Navbar.vue";
 import About from "@/components/About.vue";
 import Education from "@/components/Education.vue";
 import Experience from "@/components/Experience.vue";
-
+import Projects from "@/components/Projects.vue";
 export default {
   name: "Home",
   components: {
@@ -30,7 +31,8 @@ export default {
     Navbar,
     About,
     Education,
-    Experience
+    Experience,
+    Projects
   }
 };
 </script>
