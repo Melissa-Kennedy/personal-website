059ceaf734e5f9834bb029930739f66347607df5
Melissa <melissa.4.kennedy@gmail.com>
2021-01-18 17:43:34 -0500

    Update about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,7 @@
         <img class="rounded-full" src="@/assets/melissa.jpg" />
       </div>
       <div class="w-5/6">
-        <p>A 2021 math and computer science graduate from the University of Guelph who welcomes problem solving along with challenges in programming and has a dedication for emerging technology. 7 years programming experience, working primarily in C and Python with experience in Java, Javascript and SQL. I hope to use my quick problem solving skills, deterministic work effort and skills gained as a computer science and math student to become a valuable member of your team.</p>
+        <p>A 2021 math and computer science graduate from the University of Guelph who welcomes problem solving along with challenges in programming and has a dedication for emerging technology. 7 years programming experience, working primarily in C and Python with experience in Java, Javascript, Lua and SQL. I hope to use my quick problem solving skills, deterministic work effort and skills gained as a computer science and math student to become a valuable member of your team.</p>
       </div>
     </div>
   </div>
