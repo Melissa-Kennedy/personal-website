50177773d8221d31ae9e0eaae2106be8143a3dcf
Melissa <melissa.4.kennedy@gmail.com>
2021-02-02 11:15:03 -0500

    update education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -4,13 +4,13 @@
     <h2 class="font-semibold text-xl mt-4">Univeristy of Guelph</h2>
     <div>
       <p class="text-lg">
-        <span class="italic">Bachelor Of Science</span> | September 2016- April 2021
+        <span class="italic">Bachelor Of Science, Honours</span> | September 2016- April 2021
       </p>
 
       <div class="mt-2">
-        <p>• BSc Mathematical Science</p>
-        <p>• Statistics Stream</p>
+        <p>• BSc Mathematical Science, Honours</p>
         <p>• Computer Science Emphasis</p>
+        <p>• Statistics Stream</p>
         <p>• Guelph Women in Computer Science (GWICS) – support and engage in the community of women in computer science</p>
       </div>
     </div>
